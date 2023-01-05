b7843ee40956b8d9eda3505ba83c1b4b49a12ebb
shohan-pherones <shohan.sub.56@gmail.com>
2023-01-05 23:19:30 +0600

    redux part done

--- src/features/products/cartSlice.js
+++ src/features/products/cartSlice.js
@@ -1,10 +1,10 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { toast } from "react-toastify";
 
 const initialState = {
   cartItems: localStorage.getItem("cartItems")
     ? JSON.parse(localStorage.getItem("cartItems"))
     : [],
-  cartTotalQuantity: 0,
   cartTotalAmount: 0,
 };
 
@@ -22,10 +22,32 @@ const cartSlice = createSlice({
       if (existedItemIndex >= 0) {
         // increase quantity
         state.cartItems[existedItemIndex].cartQuantity += 1;
+
+        toast.info("Quantity increased!", {
+          position: "bottom-left",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "light",
+        });
       } else {
         // add to cart
         const assembledItem = { ...action.payload, cartQuantity: 1 };
         state.cartItems.push(assembledItem);
+
+        toast.success("Product added into cart!", {
+          position: "bottom-left",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "light",
+        });
       }
 
       // add to local storage
@@ -37,12 +59,34 @@ const cartSlice = createSlice({
         (item) => item.id !== action.payload.id
       );
       state.cartItems = updatedCartItems;
+
+      toast.warn("Product removed from cart!", {
+        position: "bottom-left",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+      });
       // update local storage
       localStorage.setItem("cartItems", JSON.stringify(state.cartItems));
     },
 
     clearCart(state, action) {
       state.cartItems = [];
+
+      toast.error("Cart cleared!", {
+        position: "bottom-left",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+      });
       // update local storage
       localStorage.setItem("cartItems", JSON.stringify(state.cartItems));
     },
@@ -55,20 +99,60 @@ const cartSlice = createSlice({
       // if exist
       if (state.cartItems[itemIndex].cartQuantity > 1) {
         state.cartItems[itemIndex].cartQuantity -= 1;
+
+        toast.info("Quantity decreased!", {
+          position: "bottom-left",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "light",
+        });
       } else if (state.cartItems[itemIndex].cartQuantity === 1) {
         const updatedCartItems = state.cartItems.filter(
           (item) => item.id !== action.payload.id
         );
 
         state.cartItems = updatedCartItems;
+
+        toast.warn("Product removed from cart!", {
+          position: "bottom-left",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "light",
+        });
       }
 
       // update local storage
       localStorage.setItem("cartItems", JSON.stringify(state.cartItems));
     },
+
+    getSubtotal(state, action) {
+      const subtotal = state.cartItems.reduce((acc, item) => {
+        const { price, cartQuantity } = item;
+        const itemTotal = price * cartQuantity;
+
+        acc += itemTotal;
+
+        return acc;
+      }, 0);
+
+      state.cartTotalAmount = subtotal;
+    },
   },
 });
 
-export const { addToCart, removeFromCart, clearCart, decreaseCart } =
-  cartSlice.actions;
+export const {
+  addToCart,
+  removeFromCart,
+  clearCart,
+  decreaseCart,
+  getSubtotal,
+} = cartSlice.actions;
 export default cartSlice.reducer;
